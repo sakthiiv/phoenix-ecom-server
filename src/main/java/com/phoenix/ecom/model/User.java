09871df626efdce3dee42caea45c98861eaa7451
Rahul Ganguly <rahul.ganguly@in-RahulGanguly.local>
2018-10-22 14:49:30 +0530

    User service and category delete api change

---
+++ src/main/java/com/phoenix/ecom/model/User.java
@@ -0,0 +1,60 @@
+package com.phoenix.ecom.model;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+public class User {
+
+    @Id
+    String id;
+    String userName;
+    String emailId;
+    String role;
+    String password;
+    Boolean IsValid;
+
+    public String getId(){return id;}
+
+    public void setId(String id){this.id = id;}
+
+    public void setUserName(String s) {
+        this.userName = s;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public void setEmailId(String s) {
+        this.emailId = s;
+    }
+
+    public String getEmailId(){
+        return emailId;
+    }
+
+    public void setRole(String s) {
+        this.role = s;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public Boolean getValid() {
+        return IsValid;
+    }
+
+    public void setValid(Boolean valid) {
+        IsValid=valid;
+    }
+}
---
+++ src/test/java/com/phoenix/ecom/controller/UserControllerTest.java
@@ -0,0 +1,61 @@
+package com.phoenix.ecom.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.phoenix.ecom.model.User;
+import com.phoenix.ecom.service.user.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class UserControllerTest extends AbstractTest {
+    @Override
+    @Before
+    public void setUp(){
+        super.setUp();
+    }
+
+    @MockBean
+    private UserService userService;
+
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper=new ObjectMapper();
+            final String jsonContent=mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void shouldRegisterAUser() throws Exception{
+        User user = new User();
+        user.setUserName("user!");
+        user.setEmailId("user@user.com");
+        user.setRole("User");
+        user.setPassword("Password");
+
+
+        this.mvc.perform(post("/api/v1/user/register").contentType(MediaType.APPLICATION_JSON).content(asJsonString(user))).andDo(print()).andExpect(status().isCreated())
+                .andExpect(content().string(containsString("User created successfully")));
+
+        ArgumentCaptor<User> ac = ArgumentCaptor.forClass(User.class);
+
+        verify(userService, times(1)).createNewUser(ac.capture());
+        User value = ac.getValue();
+        assertEquals("User",value.getRole());
+
+    }
+}

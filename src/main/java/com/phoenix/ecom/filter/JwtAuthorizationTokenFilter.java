e0e85fefad504e1ed44a678171104fda51c9c633
Rahul Ganguly <rahul.ganguly@in-RahulGanguly.local>
2018-10-25 17:54:38 +0530

    [User Story Login]|[Sana Rahul]|[Commented Authorization]

--- src/main/java/com/phoenix/ecom/filter/JwtAuthorizationTokenFilter.java
+++ src/main/java/com/phoenix/ecom/filter/JwtAuthorizationTokenFilter.java
@@ -43,29 +43,30 @@ public class JwtAuthorizationTokenFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest serverReq = (HttpServletRequest) request;
-        HttpServletResponse serverRes = (HttpServletResponse)response;
-        String token  = serverReq.getHeader("Authorization");
-
-        String api = serverReq.getRequestURI();
-        String[] finalRoute = api.split("/");
-
-        if(finalRoute.length!=0 && !UNAUTHORIZED_PATHS.contains("/" + finalRoute[finalRoute.length - 1])) {
-            Claims claims=authentication.parseJWT(token);
-            String path=serverReq.getRequestURI().substring(serverReq.getContextPath().length()).replaceAll("[/]+$", "");
-            String[] apiRoute = path.split("/");
-            boolean allowedPath=AUTHORIZED_PATHS.contains("/" + apiRoute[apiRoute.length-1]);
-
-            if (claims.getSubject().equals(ADMIN) && allowedPath) {
-                chain.doFilter(request, response);
-                } else if (claims.getSubject().equals(CUSTOMER) && serverReq.getMethod().equalsIgnoreCase("GET") && allowedPath) {
-                    chain.doFilter(request, response);
-                } else {
-                    serverRes.sendError(403, "Forbidden");
-                }
-            }else{
-            chain.doFilter(request, response);
-            }
+//        HttpServletRequest serverReq = (HttpServletRequest) request;
+//        HttpServletResponse serverRes = (HttpServletResponse)response;
+//        String token  = serverReq.getHeader("Authorization");
+//
+//        String api = serverReq.getRequestURI();
+//        String[] finalRoute = api.split("/");
+//
+//        if(finalRoute.length!=0 && !UNAUTHORIZED_PATHS.contains("/" + finalRoute[finalRoute.length - 1])) {
+//            Claims claims=authentication.parseJWT(token);
+//            String path=serverReq.getRequestURI().substring(serverReq.getContextPath().length()).replaceAll("[/]+$", "");
+//            String[] apiRoute = path.split("/");
+//            boolean allowedPath=AUTHORIZED_PATHS.contains("/" + apiRoute[apiRoute.length-1]);
+//
+//            if (claims.getSubject().equals(ADMIN) && allowedPath) {
+//                chain.doFilter(request, response);
+//                } else if (claims.getSubject().equals(CUSTOMER) && serverReq.getMethod().equalsIgnoreCase("GET") && allowedPath) {
+//                    chain.doFilter(request, response);
+//                } else {
+//                    serverRes.sendError(403, "Forbidden");
+//                }
+//            }else{
+//            chain.doFilter(request, response);
+//            }
+        chain.doFilter(request,response);
 
     }
 
